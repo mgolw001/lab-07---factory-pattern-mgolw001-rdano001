d0fd76d649f7f6deaabab7d87d3c260a3326f68e
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-18 11:27:43 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include "Base.hpp"
+#include "Factory.hpp"
 #include <iostream>
 
 using namespace std;
 int main(int argc, char **argv)
 {
+	Factory f;
   	Base* expression
-	expression = parse(argv+1, argc-1);//puls one moves the pointer to the first input rather than the ./calculator, -1 corrects the length
+	expression = f.parse(argv+1, argc-1);//puls one moves the pointer to the first input rather than the ./calculator, -1 corrects the length
 	if(expression == nullptr){
 		cout<<"ERROR INVALID INPUT"<<endl;
 		return 0;
 	}
-
-	expression.stringify();
+	expression->stringify();
 	cout<<" = "<<expression.evaluate()<<endl;	
 	return 0;
 }
