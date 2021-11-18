95de8fa654977414700a1fdad6ca8b10c9fb9914
RDanover <danover.rachel@gmail.com>
2021-11-17 16:55:34 -0800

    Outlined main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include "Base.hpp"
+#include <iostream>
 
+using namespace std;
 int main(int argc, char *argv[])
 {
-
+  	Base* expression
+	expression = parse(<insert parameters>);
+	expression.stringify();
+	cout<<" = "<<expression.evaluate()<<endl;	
 	return 0;
 }
