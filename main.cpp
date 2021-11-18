1470296e830ba745dea1b8ca1f738574219f4fd4
RDanover <danover.rachel@gmail.com>
2021-11-18 11:34:40 -0800

    Fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	Factory f;
-  	Base* expression
-	expression = f.parse(argv+1, argc-1);//puls one moves the pointer to the first input rather than the ./calculator, -1 corrects the length
-	if(expression == nullptr){
-		cout<<"ERROR INVALID INPUT"<<endl;
-		return 0;
+  	Base* expression = f.parse(argv+1, argc-1);//puls one moves the pointer to the first input rather than the ./calculator, -1 corrects the length
+	if(expression!=nullptr){
+		expression->stringify();
+        	cout<<" = "<<expression->evaluate()<<endl;	
 	}
-	expression->stringify();
-	cout<<" = "<<expression.evaluate()<<endl;	
+	else{
+		cout<<"ERROR INVALID INPUT"<<endl;
+	}
 	return 0;
 }
