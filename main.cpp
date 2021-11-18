534cd610f38f44d0c886ba633c79889f5d888a86
RDanover <danover.rachel@gmail.com>
2021-11-17 17:01:38 -0800

    Added error message

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ int main(int argc, char *argv[])
 {
   	Base* expression
 	expression = parse(<insert parameters>);
+	if(expression == nullptr){
+		cout<<"ERROR INVALID INPUT"<<endl;
+		return 0;
+	}
+
 	expression.stringify();
 	cout<<" = "<<expression.evaluate()<<endl;	
 	return 0;
