ba8a453aac32375a140a9b17a14251b08672374d
RDanover <60625627+RDanover@users.noreply.github.com>
2021-11-18 11:25:13 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 using namespace std;
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
   	Base* expression
-	expression = parse(<insert parameters>);
+	expression = parse(argv+1, argc-1);//puls one moves the pointer to the first input rather than the ./calculator, -1 corrects the length
 	if(expression == nullptr){
 		cout<<"ERROR INVALID INPUT"<<endl;
 		return 0;
