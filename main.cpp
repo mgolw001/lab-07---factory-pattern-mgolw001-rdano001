a79ed0b101c6fb99346c3753f66ad7c4fd7b5485
RDanover <60625627+RDanover@users.noreply.github.com>
2021-12-01 15:46:16 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
 	Factory f;
   	Base* expression = f.parse(argv+1, argc-1);//puls one moves the pointer to the first input rather than the ./calculator, -1 corrects the length
 	if(expression!=nullptr){
-		for (int i = 1; i<=argc;i++){
+		for (int i = 1; i<argc;i++){
 			cout<<argv[i];
 		}
 		expression->stringify();
